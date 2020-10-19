b847189fe4733de47f5c79bac7fc8ad560641ff2
Enterman <43851243+Enteerman@users.noreply.github.com>
2020-10-19 19:12:15 +0800

    fix buildscript

--- build.rs
+++ build.rs
@@ -1,27 +1,21 @@
-use std::path::Component::CurDir;
-use std::path::Path;
-use std::env::{current_dir, set_current_dir};
-use std::fs::{read_dir, File};
-use std::io::Read;
+use std::env::current_dir;
+use std::fs::read_dir;
 use std::process::{Command, Child};
 
 const JAVA_SRC_DIR:  &'static str = "java";
+const OUT_DIR: &'static str = "out";
 fn main() {
-    //println!("cargo:rerun-if-changed={}", JAVA_SRC_DIR);
-
+    println!("cargo:rustc-env=JAVA_OUTPUT_DIR={}", OUT_DIR);
+    println!("cargo:rerun-if-changed={}", OUT_DIR);
     let dir = current_dir().unwrap().join(JAVA_SRC_DIR);
     let paths = read_dir(&dir).unwrap();
-    let out_dir_string = std::env::var("OUT_DIR").unwrap();
-    println!("{}", out_dir_string);
-    let out_dir = Path::new(out_dir_string.as_str());
-    set_current_dir(out_dir);
     let mut processes: Vec<Child> = vec![];
     for path in paths {
         let path = path.unwrap();
-        let process = Command::new("javac").args(&[path.path().to_str().unwrap() ,"-d", out_dir.to_str().unwrap()]).spawn().unwrap();
+        let process = Command::new("javac").args(&[path.path().to_str().unwrap() ,"-d", OUT_DIR]).spawn().unwrap();
         processes.push(process)
     }
     for mut x in processes {
-        x.wait().unwrap();
+        assert!(x.wait().unwrap().success());
     }
 }
