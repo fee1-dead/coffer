604958b2f5e3b536ee7abde83317d0a129eeb33e
Enterman <43851243+Enteerman@users.noreply.github.com>
2020-10-25 13:17:56 +0800

    clean before build

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
 use std::env::current_dir;
-use std::fs::read_dir;
+use std::fs::{read_dir, create_dir, remove_dir_all};
 use std::process::{Command, Child};
+use std::path::Path;
 
 const JAVA_SRC_DIR:  &'static str = "java";
 const OUT_DIR: &'static str = "out";
 fn main() {
     println!("cargo:rustc-env=JAVA_OUTPUT_DIR={}", OUT_DIR);
     println!("cargo:rerun-if-changed={}", OUT_DIR);
+    let out_path = Path::new(OUT_DIR);
+    if out_path.exists() {
+        remove_dir_all(out_path).expect("Clean Out Dir");
+    }
+    create_dir(out_path).expect("Create Out Directory");
     let dir = current_dir().unwrap().join(JAVA_SRC_DIR);
     let paths = read_dir(&dir).unwrap();
     let mut processes: Vec<Child> = vec![];
