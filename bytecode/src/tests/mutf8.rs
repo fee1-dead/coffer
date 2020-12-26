c3fda54541ebc1a3f9cc05d55b931d3488d38db7
Deadbeef <ent3rm4n@gmail.com>
2020-12-26 18:53:29 +0800

    fix test errors for modified utf-8

--- bytecode/src/tests/mutf8.rs
+++ bytecode/src/tests/mutf8.rs
@@ -22,7 +22,7 @@ fn test_to_modified_utf8_supplementary() {
     let emoji = "\u{1F600}";
     // 11101101 10100000 10111101 11101101 10111000 10000000
     // 1101_100000_111101
-    assert_eq!(string_to_modified_utf8(emoji).unwrap(), vec![0b11101101, 0b10100000, 0b10111101, 0b11101101, 0b10111000, 0b10000000]);
+    assert_eq!(string_to_modified_utf8(emoji), vec![0b11101101, 0b10100000, 0b10111101, 0b11101101, 0b10111000, 0b10000000]);
 }
 
 #[test]
@@ -35,7 +35,7 @@ fn test_to_modified_utf8_3bytes() {
     // 11111111_00110100
     let full_width_t = "Ｔ";
     // 11101111 10111100 10110100
-    assert_eq!(string_to_modified_utf8(full_width_t).unwrap(), vec![0b11101111, 0b10111100, 0b10110100])
+    assert_eq!(string_to_modified_utf8(full_width_t), vec![0b11101111, 0b10111100, 0b10110100])
 }
 
 #[test]
@@ -46,10 +46,10 @@ fn test_from_modified_utf8_3bytes() {
 #[test]
 fn test_to_modified_utf8_2bytes() {
     let null = "\u{0000}";
-    assert_eq!(string_to_modified_utf8(null).unwrap(), vec![0b11000000, 0b10000000]);
+    assert_eq!(string_to_modified_utf8(null), vec![0b11000000, 0b10000000]);
     // 00000011_10101001
     let omega = "Ω";
-    assert_eq!(string_to_modified_utf8(omega).unwrap(), vec![0b11001110,0b10101001]);
+    assert_eq!(string_to_modified_utf8(omega), vec![0b11001110,0b10101001]);
 }
 
 #[test]
@@ -61,5 +61,5 @@ fn test_from_modified_utf8_2bytes() {
 #[test]
 fn test_to_modified_utf8_normal() {
     let alphabet = "abcde";
-    assert_eq!(string_to_modified_utf8(alphabet).unwrap(), vec!['a', 'b', 'c', 'd', 'e'].iter().map(|&c| c as u8).collect::<Vec<u8>>())
+    assert_eq!(string_to_modified_utf8(alphabet), vec!['a', 'b', 'c', 'd', 'e'].iter().map(|&c| c as u8).collect::<Vec<u8>>())
 }
