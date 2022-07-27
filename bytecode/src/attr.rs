79ef8c4f04dfcf3f0226cffd9d3a521cc4ca514a
Deadbeef <ent3rm4n@gmail.com>
2022-07-27 23:09:26 +0800

    Remove license header

--- bytecode/src/attr.rs
+++ bytecode/src/attr.rs
@@ -1,19 +1,3 @@
-/*
- *     This file is part of Coffer.
- *
- *     Coffer is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     Coffer is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with Coffer. (LICENSE.md)  If not, see <https://www.gnu.org/licenses/>.
- */
 use crate::annotation::{Annotation, ClassTypeAnnotation};
 use crate::mod_utf8::{modified_utf8_to_string, string_to_modified_utf8};
 use crate::module::Module;
--- bytecode/src/cp.rs
+++ bytecode/src/cp.rs
@@ -1,20 +1,4 @@
 //! This module contains structures representing a constant pool and its entries.
-/*
- *     This file is part of Coffer.
- *
- *     Coffer is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU Lesser General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
- *
- *     Coffer is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with Coffer. (LICENSE.md)  If not, see <https://www.gnu.org/licenses/>.
- */
 
 use crate::{mod_utf8, prelude::{BootstrapMethod, LazyBsm, Read, Result, Write}};
 use crate::{ConstantPoolReader, ConstantPoolWriter, Error, ReadWrite};
